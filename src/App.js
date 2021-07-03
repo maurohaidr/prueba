175cda4f5a68f7709dfc9b73c8d6be205db7de55
Mauro Haidr <maurohaidr@gmail.com>
2021-07-03 16:33:22 -0300

    commit de juana

--- src/App.js
+++ src/App.js
@@ -15,9 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-          Mauro
-          Juana
+        Juana
         </a>
       </header>
     </div>
